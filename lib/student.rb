4494816ea46fc57679b5d41e662b5771fcb2ef47
Trish Ogen <patriciaogen@gmail.com>
2019-07-14 18:02:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,4 +53,12 @@ class Student
     student = Student.new(name, grade)
     student.save
   end
+
+  def self.new_from_db(row)
+    student = self.new
+    student.id = row[0]
+    student.name =  row[1]
+    student.grade = row[2]
+    student
+  end
 end
