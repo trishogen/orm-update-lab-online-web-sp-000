c0965290a85e603b1a9e56ba9d707b33103e1d85
Trish Ogen <patriciaogen@gmail.com>
2019-07-14 18:03:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,7 @@
 require_relative "../config/environment.rb"
 
 class Student
-  attr_accessor :name, :grade
-  attr_reader :id
+  attr_accessor :name, :grade, :id
 
   def initialize(name, grade)
     @id = nil
