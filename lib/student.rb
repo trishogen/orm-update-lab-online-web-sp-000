eb333d79cf6c77c47b49ab3fa8b9f7ec38e37f95
Trish Ogen <patriciaogen@gmail.com>
2019-07-14 18:08:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,6 +61,7 @@ class Student
 
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
-    DB[:conn].execute(sql, name)
+    row = DB[:conn].execute(sql, name)[0]
+    self.new_from_db(row)
   end
 end
